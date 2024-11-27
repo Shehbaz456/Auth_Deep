02d97bdea2c5d5ebcf7fbae46bc47a1be33eefec
Shehbaz <mdshehbaz666666@gmail.com>
2024-11-28 00:25:44 +0530

    Check-auth Complete backend

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,17 @@
 // const express = require("express");
 import dotenv from "dotenv"
 dotenv.config();
+import cookieParser from "cookie-parser";
 import express from "express";
-const app = express()
 import { connectDB } from "./db/connectDB.js";
 import authRouter from "./routers/auth.route.js"
 
+const app = express()
 const PORT = process.env.PORT || 8000;
 // console.log( process.env.PORT);
 
-app.use(express.json());
-
+app.use(express.json()); //  allows us to parse incoming requests: req.body
+app.use(cookieParser()); // allows us to parse incoming cookies  
 
 app.use("/api/auth",authRouter)
 
