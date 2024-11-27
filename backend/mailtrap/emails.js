df2231a4f41dcb9707423e90b646f644a7c1ddf5
Shehbaz <mdshehbaz666666@gmail.com>
2024-11-27 15:02:46 +0530

    Verifxication Email with OTP End Backend

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -1,3 +1,4 @@
+// import { Error } from "mongoose";
 import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js";
 import { mailtrpClient,sender } from "./mailtrap.config.js"
 
@@ -17,6 +18,26 @@ export const sendVerificationEmail = async (email,verificationToken) =>{
     } catch (error) {
         console.log(`Error sending Verification eamil: ${error}`);
         
-        throw new Erorr(`Error sending Verification eamil: ${error}`)
+        throw new Error(`Error sending Verification eamil: ${error}`)
     }
 }
+
+export const sendWelcomeEmail = async (email,name)=>{
+    const recipient = [{email}];
+    try {
+        
+        const response = await mailtrpClient.send({
+            from:sender,
+            to:recipient,
+            template_uuid:"3704350d-ebc2-40b9-a8c9-b0e49e459fe2",
+            template_variables:{
+                name: name,    
+            }
+        })
+        console.log("welcome email send successfully",response)
+
+    } catch (error) {
+        console.error("Error sending welcome email",error);
+        throw new Error(`Error sending welcome eamil: ${error}`)
+    }
+}                                                            
